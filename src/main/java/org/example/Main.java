10957e7c3b28fb090d07b9b2bdc7a9fa32a43a24
teemu <teemu.pulli@metropolia.fi>
2024-03-17 21:46:55 +0200

    a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,14 @@ package org.example;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    static double FartoCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+    public static float KelvinToCelsius(float kelvin) {
+        return (kelvin - 273.15f);
+    }
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println(FartoCelsius(100));
+        System.out.println(KelvinToCelsius(100));
         }
     }
-}
--- src/test/java/TestsJava.java
+++ src/test/java/TestsJava.java
@@ -1,27 +1,27 @@
+package org.example;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class TemperatureConversionTest {
+public class TestsJava {
 
     @Test
-    public void testFahrenheitToCelsius() {
-        // Test for freezing point of water
-        assertEquals(0.0, TemperatureConversion.FartoCelsius(32), 0.01);
+    public void testFartoCelsius() {
         // Test for boiling point of water
-        assertEquals(100.0, TemperatureConversion.FartoCelsius(212), 0.01);
+        assertEquals(37.77777777777778, Main.FartoCelsius(100), 0.01);
+        // Test for freezing point of water
+        assertEquals(0.0, Main.FartoCelsius(32), 0.01);
         // Test for a random temperature
-        assertEquals(20.0, TemperatureConversion.FartoCelsius(68), 0.01);
+        assertEquals(20.0, Main.FartoCelsius(68), 0.01);
     }
 
     @Test
     public void testKelvinToCelsius() {
-        // Test for absolute zero
-        assertEquals(-273.15, TemperatureConversion.KelvinToCelsius(0), 0.01);
-        // Test for freezing point of water
-        assertEquals(0.0, TemperatureConversion.KelvinToCelsius(273.15), 0.01);
         // Test for boiling point of water
-        assertEquals(100.0, TemperatureConversion.KelvinToCelsius(373.15), 0.01);
+        assertEquals(-173.15, Main.KelvinToCelsius(100), 0.01);
+        // Test for freezing point of water
+        assertEquals(0.0, Main.KelvinToCelsius(273.15f), 0.01);
         // Test for a random temperature
-        assertEquals(26.85, TemperatureConversion.KelvinToCelsius(300), 0.01);
+        assertEquals(26.85, Main.KelvinToCelsius(300), 0.01);
     }
 }
